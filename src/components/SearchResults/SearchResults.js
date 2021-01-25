ec573d3ccee10489f1af40e453c480ae83b5fe14
Ilya Loginov <linsykt@gmail.com>
2021-01-25 07:33:35 +0300

    searchlist 1

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import { Card, Col, Row } from 'antd';
 import TMDBService from '../../services/TMDBService';
-import TEST from '../../services/test';
 
+
+
+const buildCard = (data) => {
+  const parsed = {
+    backdropPath: data.backdrop_path,
+    genres: data.genre_ids,
+    id: data.id, 
+    originalLanguage: data.original_language,
+    originalTitle: data.original_title,
+    overview: data.overview,
+    popularity: data.popularity,
+    posterPath: data.poster_path,
+    releaseDate: data.release_date,
+    title: data.title,
+    video: data.video,
+    voteAverage: data.vote_average,
+    voteCount: data.vote_count,
+  };
+  
+  return (
+    <Col span={12} key={`m${parsed.id}`}>
+      <Card title="" bordered={false}>
+        { parsed.toString() }
+      </Card>
+    </Col>);
+}
 
 const SearchResults = () => {
-  const searchResultsList = [
-  (<Col span={8} key="m1">
-    <Card title="Card title" bordered={false}>
-      Card content {TEST}
-    </Card>
-  </Col>), (<Col span={8} key="m2">
-    <Card title="Card title" bordered={false}>
-      Card content {TEST}
-    </Card>
-  </Col>), (<Col span={8} key="m3">
-    <Card title="Card title" bordered={false}>
-      Card content {TEST}
-    </Card>
-  </Col>), (<Col span={8} key="m4">
-    <Card title="Card title" bordered={false}>
-      Card content {TEST}
-    </Card>
-  </Col>)
-  ];
-
+  const searchResultsList = [];
   const mdb = new TMDBService();
+
   mdb.getMovies('future').then((body) => {
-    console.log(body.results);
+    body.results.forEach((element) => {
+      searchResultsList.push( buildCard(element) );
+    });
   });
 
   return (<Row gutter={16}>
